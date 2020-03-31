e3f5c79b8d6bb3e741a56bbcfd304fab3d5a6400
MacBook <Mac@MacBook-Pro-MacBook-2.local>
2020-03-31 16:15:24 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header/Header'
 import Main from './Main/Main'
+import Popup from './Popup/Popup'
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Popup/>
     </div>
   );
 }
--- src/Header/Head.jsx
+++ src/Header/Head.jsx
@@ -4,25 +4,25 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-class Head extends React.Component {
-  render() {
-    return (
-      <div className="head">
-        <button className="head-create add-button">
-          <FontAwesomeIcon icon={faPlus} />
-          Create
-        </button>
-        <button className="head-today">Today</button>
-        <span className="head-prev">
-          <FontAwesomeIcon icon={faAngleLeft} />
-        </span>
-        <span className="head-next">
-          <FontAwesomeIcon icon={faAngleRight} />
-        </span>
-        <div className="head__row">1</div>
-      </div>
-    );
-  }
-}
+const Head = ({ showPopUp, getNextWeek, getPrevWeek, getCurrentDay }) => {
+  return (
+    <div className="head">
+      <button className="head-create add-button" onClick={() => showPopUp()}>
+        <FontAwesomeIcon icon={faPlus} />
+        Create
+      </button>
+      <button className="head-today" onClick={() => getCurrentDay()}>
+        Today
+      </button>
+      <span className="head-prev" onClick={() => getPrevWeek()}>
+        <FontAwesomeIcon icon={faAngleLeft} />
+      </span>
+      <span className="head-next" onClick={() => getNextWeek()}>
+        <FontAwesomeIcon icon={faAngleRight} />
+      </span>
+      <div className="head__row">1</div>
+    </div>
+  );
+};
 
 export default Head;
